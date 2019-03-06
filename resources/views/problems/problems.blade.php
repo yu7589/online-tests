5624d0d271f06121f41008182aed817470152abc
yu7589 <865268095@qq.com>
2019-03-06 18:56:35 +0800

    修改了图库界面，增加了一些新面板

--- resources/views/problems/problems.blade.php
+++ resources/views/problems/problems.blade.php
@@ -2,43 +2,54 @@
 
 @section('content')
 <div class="container">
-    <div class="card col-md-12 m-1">
-        <div class="card-body row">
-            <div class="dropdown">
-                <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
-                    全部题目 
-                </button>
-                &nbsp; 
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">全部题目</a>
-                    <a class="dropdown-item" href="#">已通过题目</a>
-                    <a class="dropdown-item" href="#">未通过题目</a>
+    <div class="row">
+        <div class="card col-md-12">
+            <div class="card-body row">
+                <div class="dropdown mr-2">
+                    <button type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown">
+                        全部题目 
+                    </button>
+
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">全部题目</a>
+                        <a class="dropdown-item" href="#">已通过题目</a>
+                        <a class="dropdown-item" href="#">未通过题目</a>
+                    </div>
                 </div>
-            </div>
-            
-            <!-- | 分隔符 -->
-            <div class="border-right">
+                
+                <!-- | 分隔符 -->
+                <div class="border-right mr-2">
+                </div>
+
+                <span class="text-success mt-1 ml-1" style="font-size:19px"> 
+                    默认排序
+                </span>
+                <span class="mt-1 ml-3" style="font-size:19px"> 
+                    难度排序
+                </span>
+                <span class="glyphicon glyphicon-sort-by-attributes">
+                    
+                </span>
             </div>
         </div>
     </div>
-
-    <div class="row col-md-12">
+    
+    <div class="row mt-3">
         <!-- 题目 -->
-        <div class="card col-md-8 m-1">
-            <div class="card-body row">
-                题目
+        <div class="card col-md-8">
+            <div class="card-body">
+                题目位置
             </div>
         </div>
 
         <!-- 筛选 -->
 
-        <div class="card col-md-3 m-1">
-            <div class="card-body row">
-                筛选
+        <div class="card col-md-3" style="margin-left:95px">
+            <div class="card-body">
+                筛选位置
             </div>
         </div>
     <div>
-
 </div>
 @endsection
 
