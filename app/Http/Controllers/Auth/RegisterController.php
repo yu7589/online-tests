669820d79fc29ecdd9b1ed2561cf6b45329ca883
yu7589 <865268095@qq.com>
2019-04-10 02:54:48 +0800

    修改了注册系统
    加入了学号注册，把部分英文文本修改为中文。

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'student_number' => $data['student_number'],
             'password' => Hash::make($data['password']),
         ]);
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header"></div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    你已经成功注册！
                 </div>
             </div>
         </div>
