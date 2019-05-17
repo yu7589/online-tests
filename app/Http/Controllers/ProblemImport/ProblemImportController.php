506310e52239622fe4f443396fdb91e392860635
yu7589 <865268095@qq.com>
2019-05-17 19:59:13 +0800

    修改文件上传路径为public/figure

--- app/Http/Controllers/ProblemImport/ProblemImportController.php
+++ app/Http/Controllers/ProblemImport/ProblemImportController.php
@@ -72,128 +72,131 @@ class ProblemImportController extends Controller
         $classname = $request->classname;
 
     	if ($request->isMethod('POST')) { //判断是否是POST上传，应该不会有人用get吧，恩，不会的
-    		//查看上传文件的属性
+            //查看上传文件的属性
             $fileCharacter = $request->file('source');
-            
-            $str = " ";
-            $str = str_replace(array("QU -", "SO -", "QF -", "SF -", "AN -", "CO -"), "SPLIT", $fileCharacter->get());
-            //dd($str);
-            //dd(explode("\n",$fileCharacter->get()));
-            //将题库文件按分行符划分为数组保存到problems中
-            //$newProblems = explode("\n",$fileCharacter->get());
-            $newProblems = explode("SPLIT",$str);
-            //dd($newProblems);
-            $chapter = null;
-            $section = null;
-            $stem = null;
-            $picture_url1 = null;
-            $picture_url2 = null;
-            $answer = null;
-            $explanation = null;
-            $type = null;
-            $difficulty = null;
-            $author = null;
-            $used = null;
-            $CO = null;
-
-            $count = 1;
-            for($i=1; $i<count($newProblems); $i++){
-                switch($count){
-                    case 1:
-                        $stem = trim($newProblems[$i]);
-                        //dd($stem);
-                        $count++;
-                        break;
-                    case 2:
-                        $answer = trim($newProblems[$i]);
-                        //dd($answer);
-                        $count++;
-                        break;
-                    case 3:
-                        $picture_url1 = trim($newProblems[$i]);
-                        //dd($picture_url1);
-                        $count++;
-                        break;
-                    case 4:
-                        $picture_url2 = trim($newProblems[$i]);
-                        //dd($picture_url2);
-                        $count++;
-                        break;
-                    case 5:
-                        $explanation = trim($newProblems[$i]);
-                        //dd($explanation);
-                        $count++;
-                        break;
-                    case 6:
-                        $CO = trim($newProblems[$i]);
-                        $count = 1;
-                        //dd($CO);
-
-                        $cut = explode("-", $CO);
-                        //dd($cut);
-                        $chapter = $cut[0];
-                        //dd($chapter);
-                        $section = $cut[1];
-                        //dd($section);
-                        $type = $cut[3];
-                        $difficulty = $cut[4];
-                        $author = $cut[5];
-                        $used = $cut[7];
-                        if($used){
-                            $used = 1;
-                        }else {
-                            $used = 0;
-                        }
-                        //dd($used);
-                        
-
-                        //插入新题目
-                        $problem = new Problem;
-                        $problem->classname = $request->classname;
-                        $problem->chapter = $chapter;
-                        //dd($chapter);
-                        $problem->section = $section;
-                        $problem->stem = $stem;
-                        $problem->picture_url1 = $picture_url1;
-                        $problem->picture_url2 = $picture_url2;
-                        $problem->answer = $answer;
-                        $problem->explanation = $explanation;
-                        $problem->type = $type;
-                        $problem->difficulty = $difficulty;
-                        $problem->author = $author;
-                        $problem->used = $used;
-                        $problem->save();
-
-                        //dd($problem->id);
-                        $problemState = new ProblemState;
-                        $prolem = Problem::where(['']);
-                        $problemState->problem_id = $problem->id;
-                        $problemState->correct_submit = 0;
-                        $problemState->passing_rate = 0;
-                        $problemState->all_submit = 0;
-                        $problemState->save();
-
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            /*
-    		if ($fileCharacter->isValid()) { 
+            if ($fileCharacter->isValid()) { 
     			//获取文件的扩展名 
     			$ext = $fileCharacter->getClientOriginalExtension();
+
+                if($ext == 'md'){
+                    $str = " ";
+                    $str = str_replace(array("QU -", "SO -", "QF -", "SF -", "AN -", "CO -"), "SPLIT", $fileCharacter->get());
+                    //dd($str);
+                    //dd(explode("\n",$fileCharacter->get()));
+                    //将题库文件按分行符划分为数组保存到problems中
+                    //$newProblems = explode("\n",$fileCharacter->get());
+                    $newProblems = explode("SPLIT",$str);
+                    //dd($newProblems);
+                    $chapter = null;
+                    $section = null;
+                    $stem = null;
+                    $picture_url1 = null;
+                    $picture_url2 = null;
+                    $answer = null;
+                    $explanation = null;
+                    $type = null;
+                    $difficulty = null;
+                    $author = null;
+                    $used = null;
+                    $CO = null;
+
+                    $count = 1;
+                    for($i=1; $i<count($newProblems); $i++){
+                        switch($count){
+                            case 1:
+                                $stem = trim($newProblems[$i]);
+                                //dd($stem);
+                                $count++;
+                                break;
+                            case 2:
+                                $answer = trim($newProblems[$i]);
+                                //dd($answer);
+                                $count++;
+                                break;
+                            case 3:
+                                $picture_url1 = trim($newProblems[$i]);
+                                //dd($picture_url1);
+                                $count++;
+                                break;
+                            case 4:
+                                $picture_url2 = trim($newProblems[$i]);
+                                //dd($picture_url2);
+                                $count++;
+                                break;
+                            case 5:
+                                $explanation = trim($newProblems[$i]);
+                                //dd($explanation);
+                                $count++;
+                                break;
+                            case 6:
+                                $CO = trim($newProblems[$i]);
+                                $count = 1;
+                                //dd($CO);
+
+                                $cut = explode("-", $CO);
+                                //dd($cut);
+                                $chapter = $cut[0];
+                                //dd($chapter);
+                                $section = $cut[1];
+                                //dd($section);
+                                $type = $cut[3];
+                                $difficulty = $cut[4];
+                                $author = $cut[5];
+                                $used = $cut[7];
+                                if($used){
+                                    $used = 1;
+                                }else {
+                                    $used = 0;
+                                }
+                                //dd($used);
+                                
+
+                                //插入新题目
+                                $problem = new Problem;
+                                $problem->classname = $request->classname;
+                                $problem->chapter = $chapter;
+                                //dd($chapter);
+                                $problem->section = $section;
+                                $problem->stem = $stem;
+                                $problem->picture_url1 = $picture_url1;
+                                $problem->picture_url2 = $picture_url2;
+                                $problem->answer = $answer;
+                                $problem->explanation = $explanation;
+                                $problem->type = $type;
+                                $problem->difficulty = $difficulty;
+                                $problem->author = $author;
+                                $problem->used = $used;
+                                $problem->save();
+
+                                //dd($problem->id);
+                                $problemState = new ProblemState;
+                                $prolem = Problem::where(['']);
+                                $problemState->problem_id = $problem->id;
+                                $problemState->correct_submit = 0;
+                                $problemState->passing_rate = 0;
+                                $problemState->all_submit = 0;
+                                $problemState->save();
+
+                                break;
+                            default:
+                                break;
+                        }
+                    }       
+                }
+                else if($ext == 'png'){
+                    //获取文件的绝对路径
+    			    $path = $fileCharacter->getRealPath();
  
-    			//获取文件的绝对路径
-    			$path = $fileCharacter->getRealPath();
+    			    //定义文件名
+    			    $filename = $fileCharacter->getClientOriginalName();
  
-    			//定义文件名
-    			$filename = $fileCharacter->getClientOriginalName();
- 
-    			//存储文件。disk里面的public。总的来说，就是调用disk模块里的public配置
-    			Storage::disk('public')->put($filename, file_get_contents($path));
+    			    //存储文件。disk里面的public。总的来说，就是调用disk模块里的public配置
+    			    Storage::disk('public')->put($filename, file_get_contents($path));
+                }
+                else{
+                    return redirect('problemImport')->with('status', '请上传md或png格式的文件');
+                }
             }
-            */
         }
         return redirect('problemImport')->with('status', '添加成功');
     }
