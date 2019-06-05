3c743ee1bb787285274011e7e18a7e65530cf645
yu7589 <865268095@qq.com>
2019-06-06 03:25:36 +0800

    改homework

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/problems', 'Problems\ProblemsController@index')->name('problems');
 Route::post('/problems', 'Problems\ProblemsController@store')->name('problems.store');
 Route::get('/answered','Problems\ProblemsController@show')->name('problems.show');
-Route::get('/problems/homework','Problems\ProblemsController@showHomework')->name('problems.showHomework');
+Route::get('/problems_homework','Problems\ProblemsController@showHomework')->name('problems.showHomework');
 
 //题目提交界面路由
 Route::get('/submit', 'Problems\SubmitController@index')->name('submit');
